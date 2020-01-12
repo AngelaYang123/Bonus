fc07e2338aae3c06438c431847f548797d626bec
AngelaYang123 <59731886+AngelaYang123@users.noreply.github.com>
2020-01-12 23:45:25 +0800

    Add files via upload

---
+++ math1_35.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int year;
+  while(scanf("%d",&year)==1){
+    if(year%4==0 && year%100!=0)
+      printf("Bissextile Year\n");
+    else if(year%400==0)
+      printf("Bissextile Year\n");
+    else
+      printf("Common Year\n");
+  }
+  return 0;
+}
